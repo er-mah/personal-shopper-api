c119b537a3b891d3917a4ec54314afaf841326be
Esteban <estebanarivasv@gmail.com>
2022-06-28 18:51:28 -0300

    feat: post vehicle method working

--- src/main/java/com/mah/personalshopper/controller/VehicleController.java
+++ src/main/java/com/mah/personalshopper/controller/VehicleController.java
@@ -1,12 +1,14 @@
 package com.mah.personalshopper.controller;
 
+import com.mah.personalshopper.dto.ResponseDto;
+import com.mah.personalshopper.dto.VehicleDto;
 import com.mah.personalshopper.model.Vehicle;
 import com.mah.personalshopper.service.VehicleService;
 import com.mah.personalshopper.util.ControllerConstants;
+
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(ControllerConstants.VEHICLE)
@@ -17,9 +19,12 @@ public class VehicleController {
         this.service = service;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Vehicle>> getAll() throws RuntimeException {
-        return service.getAll();
+    @ApiOperation(
+            value = "Persists a new vehicle"
+    )
+    @PostMapping("")
+    public ResponseEntity<ResponseDto<VehicleDto>> addNewVehicle(@RequestBody VehicleDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
     }
 
 }
--- src/main/java/com/mah/personalshopper/service/VehicleService.java
+++ src/main/java/com/mah/personalshopper/service/VehicleService.java
@@ -1,23 +1,34 @@
 package com.mah.personalshopper.service;
 
+import com.mah.personalshopper.mapper.VehicleMapper;
+import com.mah.personalshopper.dto.ResponseDto;
+import com.mah.personalshopper.dto.VehicleDto;
 import com.mah.personalshopper.model.Vehicle;
 import com.mah.personalshopper.repository.VehicleRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class VehicleService {
 
     private final VehicleRepository repository;
+    private final VehicleMapper mapper;
 
-    public VehicleService(VehicleRepository actorRepository) {
+    public VehicleService(VehicleRepository actorRepository, VehicleMapper mapper) {
         this.repository = actorRepository;
+        this.mapper = mapper;
     }
 
-    public ResponseEntity<List<Vehicle>> getAll() throws RuntimeException {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    public ResponseDto<VehicleDto> create(VehicleDto dto) {
+        try {
+            Vehicle fromDto = mapper.dtoToVehicle(dto);
+            VehicleDto vehicleToDto = mapper.vehicleToDto(repository.save(fromDto));
+
+            return new ResponseDto<>(HttpStatus.CREATED, "", vehicleToDto);
+        } catch (Exception e) {
+            return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR, e.toString(), null);
+        }
+
     }
 }
