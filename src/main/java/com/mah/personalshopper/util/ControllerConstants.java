1dca9d4d59b8210f914110526b832bd59691bb5d
Esteban <estebanarivasv@gmail.com>
2022-08-01 10:20:03 -0300

    feat: add url constants

--- src/main/java/com/mah/personalshopper/util/ControllerConstants.java
+++ src/main/java/com/mah/personalshopper/util/ControllerConstants.java
@@ -1,7 +1,12 @@
 package com.mah.personalshopper.util;
 
 public class ControllerConstants {
-    public static final String MAH_URI = "https://www.miautohoy.com.ar/api/internal/v01";
+    public static final String MAH_BASE_URI = "https://www.miautohoy.com.ar/api/internal/v01";
+    public static final String INFOAUTO_BASE_URI = "https://api.infoauto.com.ar/cars/pub";
+    public static final String INFOAUTO_AUTH_URI = "https://api.infoauto.com.ar/cars/auth";
+
     public static final String VEHICLE = "/vehicle";
+
+
 
 }
