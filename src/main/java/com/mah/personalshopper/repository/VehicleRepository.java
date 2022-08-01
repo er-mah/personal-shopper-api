8bb34c7d267bfd4c1194b46538036ef43221a572
Esteban <estebanarivasv@gmail.com>
2022-08-01 10:19:39 -0300

    refactor: delete unused methods

--- src/main/java/com/mah/personalshopper/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mah.personalshopper.repository;
-
-import com.mah.personalshopper.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
-}
